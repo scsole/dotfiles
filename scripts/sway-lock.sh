776a8dc7366a34da222acce9dcfb23b3693f975b
Sebastian Sole <scsole@protonmail.com>
2020-08-22 17:39:51 +1200

    Use custom sway-lock script

---
+++ scripts/sway-lock.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+ 
+# Dependencies:
+# imagemagick
+# swaylock
+# grim
+# corrupter (https://github.com/r00tman/corrupter)
+
+IMAGE=/tmp/swaylock.png
+LOCK=~/stow/bin/assets/stop.png
+LOCKARGS=""
+
+for OUTPUT in `swaymsg -t get_outputs | jq -r '.[] | select(.active == true) | .name'`
+do
+    IMAGE=/tmp/$OUTPUT-lock.png
+    grim -o $OUTPUT $IMAGE
+    corrupter $IMAGE $IMAGE
+    composite -gravity center $LOCK $IMAGE $IMAGE
+    LOCKARGS="${LOCKARGS} --image ${OUTPUT}:${IMAGE}"
+    IMAGES="${IMAGES} ${IMAGE}"
+done
+swaylock $LOCKARGS
+rm $IMAGES
